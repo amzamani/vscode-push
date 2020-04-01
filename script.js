55b49b8ad0ba654646c2fcdd49846395040fdfbe
amzamani <62957837+amzamani@users.noreply.github.com>
2020-04-01 06:01:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ e3.x = 366
 function isCollided(box1, box2) {
   console.log(box1.x)
   console.log(box2.x)
-if ((box2.x+50) == box1.x )return true;
+if ((box2.y+50) == box1.y )return true;
     // fill this yourself
     
 }
